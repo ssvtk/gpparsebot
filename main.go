caee617df4bcbfe173481e35ae15f2f06363d45a
Slava Tkachenko <49266473+ssvtk@users.noreply.github.com>
2021-01-15 22:38:27 +0700

    Small changes concerning logging.

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/jackc/pgx"
 	"gpparstel/db"
 	"gpparstel/parser"
 	"gpparstel/telegram"
 	"log"
-	"os"
 	"time"
 )
 
@@ -20,8 +18,7 @@ func main() {
 
 	log.Println("Connection successfully done")
 	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 	defer conn.Close()
 
